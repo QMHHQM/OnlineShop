c2ed697ce159f884f8d14c3fe122b6b7673b1db1
gurui <gurui@intra.nsfocus.com>
2017-06-30 15:44:15 +0800

    完善前端项目结构

---
+++ client/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    {{ hello }}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    data () {
+      return {
+        hello: "hi"
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
