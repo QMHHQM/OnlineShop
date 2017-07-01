ae0c1fbf7eed09823257aaf3cfab5c91f73f0f21
grapes4 <grapes4@sina.com>
2017-07-01 11:52:21 +0800

    完成fetch，更新了部分api文档

--- client/src/service/getData.js
+++ client/src/service/getData.js
@@ -1,1 +1,35 @@
-getData.js
+import fetch from '../config/fetch'
+
+/**
+ * 提交注册信息
+ */
+
+export const regist = (userId, userName, password, realName, tel, address, zip, email) => fetch('user/regist.do', {
+    userId,
+    userName,
+    password,
+    realName,
+    tel,
+    address,
+    zip,
+    email
+})
+
+/**
+ * 普通用户登录
+ */
+
+export const userLogin = (userName, password) => fetch('user/login.do', {
+    userName,
+    password
+})
+
+/**
+ * 管理员登录
+ */
+
+export const adminLogin = (userName, password) => fetch('admin/login.do', {
+    userName,
+    password
+})
+
